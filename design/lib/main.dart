abaf8dff307242ed656d5004aaec7328bf9e17c8
ErickFerrer96 <erick.ferrer1996@gmail.com>
2019-11-10 10:51:51 -0600

    Screen shoots

--- design/lib/main.dart
+++ design/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 
       debugShowCheckedModeBanner: false,
       title: 'Diseños',
-      initialRoute: 'boton',
+      initialRoute: 'basico',
 
       routes: {
 
